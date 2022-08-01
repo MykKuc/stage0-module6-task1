4562093604af6a87bcd339dfdefcf5e4241fc2a8
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-08-01 13:48:38 +0300

    Completed task.

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,37 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+            if (hasFur) {
+                if(numberOfPaws > 1 ){
+                    return "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and a fur.";
+                }else if(numberOfPaws == 1){
+                    return "This animal is mostly " + color + ". It has " + numberOfPaws + " paw and a fur.";
+                }else{
+                    return "Incorrect arguments given.";
+                }
+
+            } else {
+                if(numberOfPaws > 1) {
+                    return"This animal is mostly " + color + ". It has " + numberOfPaws + " paws and no fur.";
+                }else if (numberOfPaws == 1){
+                    return"This animal is mostly " + color + ". It has " + numberOfPaws + " paw and no fur.";
+                }else{
+                    return"Incorrect arguments given.";
+                }
+            }
+
+    }
 
 }
